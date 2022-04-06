9fe2023831a6dfbc2ed675aaa924b4e1318e8fe5
Xsodex_app <Flex513513@gmail.com>
2022-04-06 16:19:49 +0300

    Начало верстки

--- controllers/category.js
+++ controllers/category.js
@@ -1,7 +1,7 @@
 // Контроллер для getAll(получить все категории который создал пользователь)
 module.exports.getAll = async function(req, res) {
     res.status(200).json({
-        message: 'Все гуд'
+        message: 'Вы авторизованы!!!'
     });
 
 
