2bedfae86680ea6cce003de82058b58d1990452b
Xsodex_app <Flex513513@gmail.com>
2022-08-21 20:32:26 +0300

    Промежуточная подготовка деплоя

--- app.js
+++ app.js
@@ -76,6 +76,24 @@ app.use('/api/additional-like-comment', additionalLikeCommentRoutes);
 
 
 
+// Подготовка деплоя
+if(process.env.NODE_ENV === 'production')
+{
+    app.use(express.static('client/dist/client'));
+
+    app.get('*', (req,res)=> {
+        res.sendFile(
+            path.resolve(
+                __dirname, 'client', 'dist', 'client', 'index.html'
+            )
+        );
+    });
+}
+
+
+
+
+
 
 
 
---
+++ config/keys.prod.js
@@ -0,0 +1,5 @@
+// Храним ключи для продакшена
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    jwt: process.env.JWT,
+};
