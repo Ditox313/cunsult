04be001da61abc95e420011ff1b59fe47d64b2d0
Xsodex_app <Flex513513@gmail.com>
2022-08-22 08:44:10 +0300

    Исправил ошибки при подготовке к тестовой выгрузке

--- app.js
+++ app.js
@@ -77,18 +77,18 @@ app.use('/api/additional-like-comment', additionalLikeCommentRoutes);
 
 
 // Подготовка деплоя
-if(process.env.NODE_ENV === 'production')
-{
-    app.use(express.static('client/dist/client'));
+// if(process.env.NODE_ENV === 'production')
+// {
+//     app.use(express.static('client/dist/client'));
 
-    app.get('*', (req,res)=> {
-        res.sendFile(
-            path.resolve(
-                __dirname, 'client', 'dist', 'client', 'index.html'
-            )
-        );
-    });
-}
+//     app.get('*', (req,res)=> {
+//         res.sendFile(
+//             path.resolve(
+//                 __dirname, 'client', 'dist', 'client', 'index.html'
+//             )
+//         );
+//     });
+// }
 
 
 
---
+++ config/keys.dev.js
@@ -0,0 +1,5 @@
+// Храним ключи для разработки
+module.exports = {
+    mongoUri: 'mongodb+srv://xsodex_consult:19731379Consult@cluster0.bsvwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    jwt: 'xsodex_consult_jwt',
+};
