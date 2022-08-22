165cb90815a93cf7ed107fcb705197a9c580b9c2
Xsodex_app <Flex513513@gmail.com>
2022-08-22 12:02:51 +0300

    Тестовый деплой версия 7

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
-module.exports = {
-    mongoUri: 'mongodb+srv://xsodex_consult:19731379Consult@cluster0.bsvwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    jwt: 'xsodex_consult_jwt',
-};
+// module.exports = {
+//     mongoUri: 'mongodb+srv://xsodex_consult:19731379Consult@cluster0.bsvwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+//     jwt: 'xsodex_consult_jwt',
+// };
 
 
 
